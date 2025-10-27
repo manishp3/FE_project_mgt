a24ab294e1774718e5e4797dbd82f18243e38a27
Manish Patadiya <patadiyamanish20@gmail.com>
2025-10-27 11:17:02 +0530

    backend url change

--- build/assets/CDropdownDivider-PIT7FbRe.js
+++
@@ -1,1 +0,0 @@
-import{r as p,_ as t,R as c,a as i,c as d,P as m}from"./index-Ds23Eqm-.js";var a=p.forwardRef(function(s,r){var e=s.className,o=t(s,["className"]);return c.createElement("hr",i({className:d("dropdown-divider",e)},o,{ref:r}))});a.propTypes={className:m.string};a.displayName="CDropdownDivider";export{a as C};
